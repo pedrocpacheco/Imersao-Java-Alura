81359879d3d326d8863e33c323876eb8f6c3dda3
Pedro C. Pacheco <cpedropacheco@hotmail.com>
2023-03-27 17:59:16 -0300

    fazendo interface interativa e comecando a adicionar estrelinhas

--- src/App.java
+++ src/App.java
@@ -21,18 +21,37 @@ public class App {
 
         // exibir os dados
         for (Map<String,String> filme : listaFiltrada) {
-            System.out.println(filme.get("title"));
-            System.out.println(filme.get("image"));
-            System.out.println(filme.get("imDbRating"));
+            System.out.println("\u001b[1m Titulo:\u001b[m " + filme.get("title"));
+            System.out.println("\u001b[1m Imagem:\u001b[m " + filme.get("image"));
+            System.out.println("\u001b[1m Nota:\u001b[m " + filme.get("imDbRating"));
+
+            // Printando Estrelinha
+            for (int j = 1; j <= 5; j++){
+                System.out.print("⭐");
+            }
+            //Pulando Linha
+            System.out.println("\n");
+
+        
         }
 
     }
 
     public static String buscarDadosApi() throws IOException, InterruptedException{
         Scanner cc = new Scanner(System.in);
-        System.out.println("Digite a URL da API que você deseja importar:");
-        String url = cc.nextLine();
+        System.out.println("De qual API você deseja importar os dados? ");
+        System.out.println("1- Filmes | 2- Séries");
+        int opcao = cc.nextInt();
+        String url;
         cc.close();
+        if(opcao == 1){
+            url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopTVs.json";
+        } else if(opcao == 2){
+            url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        } else{
+            throw new RuntimeException("Opção Indisponivel");
+        }
+
             URI endereco = URI.create(url);
             HttpClient client = HttpClient.newHttpClient();
             HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
