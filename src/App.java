fa7c6538d137f45f29502301b2f6b582976b952b
Pedro C. Pacheco <cpedropacheco@hotmail.com>
2023-03-27 16:26:58 -0300

    terminando aula 1

--- src/App.java
+++ src/App.java
@@ -4,6 +4,9 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -13,8 +16,15 @@ public class App {
         String dadosAPI = buscarDadosApi();
 
         // extrair so os dados que interessam (titulo, poster, nota)
+        JsonParser parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(dadosAPI);
 
         // exibir os dados
+        for (Map<String,String> filme : listaDeFilmes) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+        }
 
     }
 
@@ -26,5 +36,7 @@ public class App {
             HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
             String body = response.body();
             return body;
+
+
 }
 }
