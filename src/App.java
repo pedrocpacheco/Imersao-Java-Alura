b8fad2e09326ace5da7e806daea81bfce6c2d901
Pedro C. Pacheco <cpedropacheco@hotmail.com>
2023-03-27 16:05:39 -0300

    passando a conexao para um metodo do APP

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -9,17 +10,21 @@ public class App {
         System.out.println("Pegando Informações da API:");
 
         // Fazer uma conexão http e os buscar os top 250 filmes
-        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
-        URI endereco = URI.create(url);
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
-        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-        String body = response.body();
-        System.out.println(body);
+        String dadosAPI = buscarDadosApi();
 
         // extrair so os dados que interessam (titulo, poster, nota)
 
         // exibir os dados
 
     }
+
+    public static String buscarDadosApi() throws IOException, InterruptedException{
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+            URI endereco = URI.create(url);
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
+            HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+            String body = response.body();
+            return body;
+}
 }
