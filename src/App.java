be98d25041aa980c8098eafe2993727c855617a6
Pedro C. Pacheco <cpedropacheco@hotmail.com>
2023-03-28 16:41:36 -0300

    terminando de separar por classes

--- src/App.java
+++ src/App.java
@@ -6,35 +6,58 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Pegando Informações da API:");
 
-        // Fazer uma conexão http e os buscar os top 250 filmes
+        // OBJETO Buscador
         BuscadorAPI buscadorAPI = new BuscadorAPI();
-
+        // METODO Buscar
         String dadosAPI = buscadorAPI.buscarDadosApi();
 
-        // Escolhendo metodo de Vizualização de Imagem
+        // MENU escolha de modo de imagem
         Scanner cc = new Scanner(System.in);
 
+        // Opções de modo de imagem
         System.out.println("Como você deseja vizualizar as imagens dos filmes/series?");
         System.out.println("1- ASCII | 2- JFRAME | 3- Figurinhas");
-
         int opcaoVizualicao = cc.nextInt();
-
         cc.close();
 
-        // extrair so os dados que interessam (titulo, poster, nota)
+        // OBJETO Filtador  
         FiltradorLista filtradorLista = new FiltradorLista();
-
+        // METODO Buscar
         List<Map<String, String>> listaFiltrada =  filtradorLista.filtrarLista(dadosAPI);
 
-        // exibir os dados
+        // LOOP Mostrar Infos Padrões
         for (Map<String,String> filme : listaFiltrada) {
             System.out.println("\u001b[1mTitulo:\u001b[m " + filme.get("title"));
             System.out.println("\u001b[1mImagem:\u001b[m " + filme.get("image"));
             System.out.println("\u001b[1mNota:\u001b[m " + filme.get("imDbRating"));
             
-            // Passando valor da nota de String para Int
+            // METODO Imprirmir Estrelas
             double notaEmDouble = Double.parseDouble(filme.get("imDbRating"));
-            int notaEmInt = (int) notaEmDouble;
+            imprimirEstrelas(notaEmDouble);
+            
+            // OPÇÕES de Vizualização
+            if(opcaoVizualicao == 1){
+                //OBJETO e METODO de ASCII
+                ConversorASCII conversorASCII = new ConversorASCII();
+                System.out.println(conversorASCII.imageParaAscii(filme.get("image")));
+            } else if (opcaoVizualicao == 2){
+                //OBJETO e Metodo de JFrame
+                String urlImagem = filme.get("image");
+                CriadorJFrame criadorJFrame = new CriadorJFrame();
+                criadorJFrame.criarJframe(urlImagem);
+            } else if (opcaoVizualicao == 3){
+                //OBJETO e METODO de Stickers
+                GeradorStickers geradorStickers = new GeradorStickers();
+                geradorStickers.criar(filme.get("image"), filme.get("title"), "Outra ATT");
+            } else{
+                throw new RuntimeException("Opção Inexistente");
+            }
+            }
+        }
+
+    private static void imprimirEstrelas(double notaEmDouble) {
+
+        int notaEmInt = (int) notaEmDouble;
 
             // Printando Estrelinha
             for (int j = 1; j <= notaEmInt; j++){
@@ -42,26 +65,7 @@ public class App {
             }
             //Pulando Linha
             System.out.println("\n");
-            
-            //------------------------------------------------------
-            // Forma de Vizualiar imagem referente a escolha
-            switch(opcaoVizualicao){
-                case 1: 
-                    ConversorASCII conversorASCII = new ConversorASCII();
-                    System.out.println(conversorASCII.imageParaAscii(filme.get("image")));
-                case 2:
-                    // Opcao de JFRAME
-                    String urlImagem = filme.get("image");
-                    CriadorJFrame criadorJFrame = new CriadorJFrame();
-                    criadorJFrame.criarJframe(urlImagem);
 
-                case 3:
-                    GeradorStickers geradorStickers = new GeradorStickers();
-                    geradorStickers.criar(filme.get("image"), filme.get("title"), "ATUALIZADO");
-                default:
-                    throw new RuntimeException("Opção Inexistente");
-            }
-        }
     }
     
 }
